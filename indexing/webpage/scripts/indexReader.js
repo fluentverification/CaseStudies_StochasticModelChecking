8c0c7bde52089ef191a079e208dad980a2eff619
Josh Jeppson <joshua.jeppson@usu.edu>
2022-11-07 16:19:27 -0700

    Added search suggestions

--- indexing/webpage/scripts/indexReader.js
+++ indexing/webpage/scripts/indexReader.js
@@ -1,3 +1,42 @@
+let suggestions = [
+	"foo"
+	, "bar"
+	, "baz"
+	, "foobar"
+	, "fun"
+	, "zedd"
+	, "case"
+	, "never"
+	, "gonna"
+	, "give"
+	, "you"
+	, "up"
+];
+
 function createTag() {
 	// TODO
+}
+
+function showSuggestions() {
+	let queryText = document.getElementById('query');
+	let querySuggestions = document.getElementById('query-suggestions');
+	let currentQuery = queryText.value;
+	querySuggestions.innerHTML = "";
+	if (currentQuery.length == 0) { return; }
+	suggestions.forEach(s => {
+		let locationIndex = s.indexOf(currentQuery)
+		if (locationIndex >= 0) {
+			querySuggestions.innerHTML += "<div class=suggestion onclick=\"acceptSuggestion('" + s + "')\">" + s + "</div>";
+			// console.log(s);
+		}
+		// else {
+		// 	console.log("Omitting " + s + " since query is " + currentQuery);
+		// }
+	});
+}
+
+function acceptSuggestion(suggestion) {
+	let queryText = document.getElementById('query');
+	queryText.value = suggestion;
+	showSuggestions();
 }
